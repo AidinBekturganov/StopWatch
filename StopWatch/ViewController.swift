484a125a02ba921eb2ca555668c089d39d732984
Aidin <bekturganovaidin@gmail.com>
2020-06-24 20:47:48 +0600

    Done with first stopwatch without conditions for time running(without auto-layout )

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,12 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var time = Timer()
+    var (hours, minutes, seconds) = (0, 0, 0)
+    var savedPauses: [String] = []
+    var flagForResetEnable = true
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var stopButtonAndReset: UIButton!
+    @IBOutlet weak var startButtonToEnableReset: UIButton!
+       
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        
+        
+        if time.isValid{
+            let currentTime = "\(hours):\(minutes):\(seconds) is lap №\(savedPauses.count + 1)"
+            savedPauses.append(currentTime)
+            let indexPath = IndexPath(row: savedPauses.count - 1, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        } else if flagForResetEnable{
+            time.invalidate()
+            (hours, minutes, seconds) = (0, 0, 0)
+            timeLabel.text = "00:00:00"
+            stopButtonAndReset.isEnabled = false
+            savedPauses = []
+            tableView.reloadData()
+            
+
+        }
+        
     }
+    
+  
+   
+  
+    
+    @IBAction func continueButton(_ sender: UIButton) {
+        if !time.isValid || startButtonToEnableReset.currentBackgroundImage == #imageLiteral(resourceName: "Button-Normal") {
+            time = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.keepTimer), userInfo: nil, repeats: true)
+            startButtonToEnableReset.setBackgroundImage(#imageLiteral(resourceName: "Button-Highlighted"), for: .normal)
+            stopButtonAndReset.isEnabled = true
+        } else {
+                time.invalidate()
+            flagForResetEnable = true
+            startButtonToEnableReset.setBackgroundImage(#imageLiteral(resourceName: "Button-Normal"), for: .normal)
+                
+        }
+    }
+    
+  
+    
+    @objc func keepTimer(){
+        
+        seconds += 1
+        if seconds > 60{
+            minutes += 1
+            seconds = 0
+        }
+        
+        if minutes > 60{
+            hours += 1
+            minutes = 0
+        }
+        
+        let secondString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minuteString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        let hourString = hours > 9 ? "\(hours)" : "0\(hours)"
 
-
+        timeLabel.text = "\(hourString):\(minuteString):\(secondString)"
+    }
+    
+   
+    
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedPauses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lapCell", for: indexPath)
+        cell.textLabel?.text = savedPauses[indexPath.row]
+        return cell
+    }
+    
+}
---
+++ StopWatch/WatchWithEndViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WatchWithEndViewController.swift
+//  StopWatch
+//
+//  Created by User on 6/24/20.
+//  Copyright © 2020 Aidin. All rights reserved.
+//
+
+import UIKit
+
+class WatchWithEndViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toGetBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
